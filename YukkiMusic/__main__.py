3ba95a3ad82c97fa18cb92ab6a36ef06ab39766b
Meyit47zade <166849634+Meyit47zade@users.noreply.github.com>
2024-04-16 21:24:30 +0300

    Update __main__.py

--- YukkiMusic/__main__.py
+++ YukkiMusic/__main__.py
@@ -16,13 +16,14 @@ from pytgcalls.exceptions import NoActiveGroupCall
 
 import config
 from config import BANNED_USERS
-from YukkiMusic import logging, app, userbot
+from YukkiMusic import LOGGER
+from YukkiMusic import app, userbot
 from YukkiMusic.core.call import Yukki
 from YukkiMusic.plugins import ALL_MODULES
 from YukkiMusic.utils.database import get_banned_users, get_gbanned
 
 loop = asyncio.get_event_loop()
-
+   
 
 async def init():
     if (
