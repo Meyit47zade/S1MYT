16acb25362a5cb08b10fa778c99f60ca1ebae4bc
Meyit47zade <166849634+Meyit47zade@users.noreply.github.com>
2024-04-17 08:33:45 +0300

    Update bot.py

--- YukkiMusic/core/bot.py
+++ YukkiMusic/core/bot.py
@@ -8,11 +8,11 @@ import config
 
 from ..logging import LOGGER
 
-class ArchMusic(Client):
+class YukkiMusic(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot")
         super().__init__(
-            "ArchMusic",
+            "YukkiMusic",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
