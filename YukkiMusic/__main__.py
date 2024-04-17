0488b2d7a3f39df6766a733dc97e598d71aaa800
Meyit47zade <166849634+Meyit47zade@users.noreply.github.com>
2024-04-17 10:02:03 +0300

    Update __main__.py

--- YukkiMusic/__main__.py
+++ YukkiMusic/__main__.py
@@ -52,7 +52,7 @@ async def init():
             BANNED_USERS.add(user_id)
     except:
         pass
-    await app.start()
+    await YukkiMusic.start()
     for all_module in ALL_MODULES:
         importlib.import_module("YukkiMusic.plugins" + all_module)
     LOGGER("YukkiMusic.plugins").info(
