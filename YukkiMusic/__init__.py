1664c3eebe705c3a6c14a1b0809a083366aa08e2
Meyit47zade <166849634+Meyit47zade@users.noreply.github.com>
2024-04-17 08:18:31 +0300

    Update __init__.py

--- YukkiMusic/__init__.py
+++ YukkiMusic/__init__.py
@@ -1,17 +1,8 @@
-#
-# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
-#
-# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
-#
-# All rights reserved.
-
 from YukkiMusic.core.bot import YukkiBot
 from YukkiMusic.core.dir import dirr
 from YukkiMusic.core.git import git
 from YukkiMusic.core.userbot import Userbot
-from YukkiMusic.misc import dbb, heroku, sudo
+from YukkiMusic.misc import dbb, heroku
 
 from .logging import LOGGER
 
@@ -26,9 +17,6 @@ dbb()
 
 # Heroku APP
 heroku()
-
-# Load Sudo Users from DB
-sudo()
 
 # Bot Client
 app = YukkiBot()
