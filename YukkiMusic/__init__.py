aecd24c3cee26f9383d9b1de7b91f0a86c957a32
Meyit47zade <166849634+Meyit47zade@users.noreply.github.com>
2024-04-18 00:29:57 +0300

    Update __init__.py

--- YukkiMusic/__init__.py
+++ YukkiMusic/__init__.py
@@ -1,14 +1,26 @@
+#
+# Copyright (C) 2021-2023 by ArchBots@Github, < https://github.com/ArchBots >.
+#
+# This file is part of < https://github.com/ArchBots/ArchMusic > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/ArchBots/ArchMusic/blob/master/LICENSE >
+#
+# All rights reserved.
+#
+
 from YukkiMusic.core.bot import YukkiMusic
 from YukkiMusic.core.dir import dirr
 from YukkiMusic.core.git import git
 from YukkiMusic.core.userbot import Userbot
-from YukkiMusic.misc import dbb, heroku
+from YukkiMusic.misc import dbb, heroku, sudo
 
 from .logging import LOGGER
 
 # Directories
 dirr()
 
+# Check Git Updates
+git()
 
 # Initialize Memory DB
 dbb()
@@ -16,8 +28,11 @@ dbb()
 # Heroku APP
 heroku()
 
+# Load Sudo Users from DB
+sudo()
+
 # Bot Client
-app = YukkiBot()
+app = YukkiMusic()
 
 # Assistant Client
 userbot = Userbot()
