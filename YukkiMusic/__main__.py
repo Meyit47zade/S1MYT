d5fe8ce854a36b1876a8b7abf7d377e913d074c8
Meyit47zade <166849634+Meyit47zade@users.noreply.github.com>
2024-04-17 07:19:34 +0300

    Update __main__.py

--- YukkiMusic/__main__.py
+++ YukkiMusic/__main__.py
@@ -16,7 +16,7 @@ from pytgcalls.exceptions import NoActiveGroupCall
 
 import config
 from config import BANNED_USERS
-from YukkiMusic.logging import LOGGER
+from YukkiMusic.logging import Logger
 from YukkiMusic import app, userbot
 from YukkiMusic.core.call import Yukki
 from YukkiMusic.plugins import ALL_MODULES
