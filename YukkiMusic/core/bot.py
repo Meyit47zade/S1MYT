c7b826798a904b078929d53517705f76806b5424
Meyit47zade <166849634+Meyit47zade@users.noreply.github.com>
2024-04-18 00:46:17 +0300

    Update bot.py

--- YukkiMusic/core/bot.py
+++ YukkiMusic/core/bot.py
@@ -12,7 +12,7 @@ class YukkiMusic(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot")
         super().__init__(
-            "YukkiMusic",
+            "YukkiBot",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
