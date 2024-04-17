bc8ad730f0205dbb4e8cb84f667ea714d8aab666
Meyit47zade <166849634+Meyit47zade@users.noreply.github.com>
2024-04-17 08:13:25 +0300

    Update misc.py

--- YukkiMusic/misc.py
+++ YukkiMusic/misc.py
@@ -1,11 +1,12 @@
 #
-# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
+# Copyright (C) 2021-2023 by ArchBots@Github, < https://github.com/ArchBots >.
 #
-# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
+# This file is part of < https://github.com/ArchBots/ArchMusic > project,
 # and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
+# Please see < https://github.com/ArchBots/ArchMusic/blob/master/LICENSE >
 #
 # All rights reserved.
+#
 
 import socket
 import time
