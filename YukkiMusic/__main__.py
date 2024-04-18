b037edf9fdad830402052814a633eb1a771c8ea2
Meyit47zade <166849634+Meyit47zade@users.noreply.github.com>
2024-04-18 12:43:37 +0300

    Update __main__.py

--- YukkiMusic/__main__.py
+++ YukkiMusic/__main__.py
@@ -31,12 +31,12 @@ async def init():
             BANNED_USERS.add(user_id)
     except:
         pass
-    await app.start()
+    await Yukki().start()
     for all_module in ALL_MODULES:
         importlib.import_module("YukkiMusic.plugins" + all_module)
     LOGGER("YukkiMusic.plugins").info("Successfully Imported Modules...")
     await userbot.start()
-    await Yukki.start()
+    await app.start()
     try:
         await Yukki.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
     except NoActiveGroupCall:
@@ -46,7 +46,7 @@ async def init():
         exit()
     except:
         pass
-    await Anony.decorators()
+    await Yukki.decorators()
     LOGGER("YukkiMusic").info(
         "\x41\x6e\x6f\x6e\x58\x20\x4d\x75\x73\x69\x63\x20\x42\x6f\x74\x20\x53\x74\x61\x72\x74\x65\x64\x20\x53\x75\x63\x63\x65\x73\x73\x66\x75\x6c\x6c\x79\x2e\n\n\x44\x6f\x6e'\x74\x20\x66\x6f\x72\x67\x65\x74\x20\x74\x6f\x20\x76\x69\x73\x69\x74\x20\x40\x46\x61\x6c\x6c\x65\x6e\x41\x73\x73\x6f\x63\x69\x61\x74\x69\x6f\x6e"
     )
