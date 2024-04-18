ccd03a361e44e8809c17ae50630064af0c83491c
Meyit47zade <166849634+Meyit47zade@users.noreply.github.com>
2024-04-18 16:13:08 +0300

    Update __main__.py

--- YukkiMusic/__main__.py
+++ YukkiMusic/__main__.py
@@ -76,4 +76,4 @@ async def init(self):
     await idle()
 
 if __name__ == '__main__':
-    asyncio.get_event_loop().run_until_complete(main())
+    asyncio.get_event_loop().run_until_complete(init())
