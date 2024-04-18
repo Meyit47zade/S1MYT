15fe75c4962270efd3a2653f9c73aa003867f6a1
Meyit47zade <166849634+Meyit47zade@users.noreply.github.com>
2024-04-18 09:30:06 +0300

    Add files via upload

--- YukkiMusic/core/bot.py
+++ YukkiMusic/core/bot.py
@@ -1,19 +1,27 @@
-import sys
-import pyrogram 
+#
+# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
+#
+# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
+#
+# All rights reserved.
 
-from pyrogram import Client, enums
-from pyrogram.enums import ChatMemberStatus, ChatType
+import sys
+
+from pyrogram import Client
 from pyrogram.types import BotCommand
 
 import config
 
 from ..logging import LOGGER
 
-class YukkiMusic(Client):
+
+class YukkiBot(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot")
         super().__init__(
-            "YukkiBot",
+            "YukkiMusicBot",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
@@ -21,60 +29,45 @@ class YukkiMusic(Client):
 
     async def start(self):
         await super().start()
+        get_me = await self.get_me()
+        self.username = get_me.username
+        self.id = get_me.id
         try:
-            get_me = await self.get_me()
-            self.username = get_me.username
-            self.id = get_me.id
-
-            video_url = "https://graph.org/file/f9d32b651af18e929e3d8.mp4"
-            caption = "Bot Başladı... ⛄️"
-            
+            await self.send_message(
+                config.LOG_GROUP_ID, "Bot Started"
+            )
+        except:
+            LOGGER(__name__).error(
+                "Bot has failed to access the log Group. Make sure that you have added your bot to your log channel and promoted as admin!"
+            )
+            sys.exit()
+        if config.SET_CMDS == str(True):
             try:
-                await self.send_video(
-                    config.LOG_GROUP_ID,
-                    video=video_url,
-                    caption=caption,
-                )
-            except:
-                LOGGER(__name__).error(
-                    "Bot has failed to access the log Group. Make sure that you have added your bot to your log channel and promoted as admin!"
-                )
-                sys.exit()
-
-            if config.SET_CMDS == str(True):
-                try:
-                    await self.set_bot_commands(
-                        [
-                            BotCommand("ping", "Check that bot is alive or dead"),
-                            BotCommand("play", "Starts playing the requested song"),
-                            BotCommand("skip", "Moves to the next track in queue"),
-                            BotCommand("pause", "Pause the current playing song"),
-                            BotCommand("resume", "Resume the paused song"),
-                            BotCommand("end", "Clear the queue and leave voice chat"),
-                            BotCommand("shuffle", "Randomly shuffles the queued playlist."),
-                            BotCommand("playmode", "Allows you to change the default playmode for your chat"),
-                            BotCommand("settings", "Open the settings of the music bot for your chat.")
+                await self.set_bot_commands(
+                    [
+                        BotCommand("ping", "Check that bot is alive or dead"),
+                        BotCommand("play", "Starts playing the requested song"),
+                        BotCommand("skip", "Moves to the next track in queue"),
+                        BotCommand("pause", "Pause the current playing song"),
+                        BotCommand("resume", "Resume the paused song"),
+                        BotCommand("end", "Clear the queue and leave voice chat"),
+                        BotCommand("shuffle", "Randomly shuffles the queued playlist."),
+                        BotCommand("playmode", "Allows you to change the default playmode for your chat"),
+                        BotCommand("settings", "Open the settings of the music bot for your chat.")
                         ]
                     )
-                except:
-                    pass
-            else:
+            except:
                 pass
-
-            a = await self.get_chat_member(config.LOG_GROUP_ID, self.id)
-            if a.status != ChatMemberStatus.ADMINISTRATOR:
-                LOGGER(__name__).error(
-                    "Please promote Bot as Admin in Logger Group"
-                )
-                sys.exit()
-
-        except Exception as e:
-            LOGGER(__name__).error(f"Error during bot start: {e}")
+        else:
+            pass
+        a = await self.get_chat_member(config.LOG_GROUP_ID, self.id)
+        if a.status != "administrator":
+            LOGGER(__name__).error(
+                "Please promote Bot as Admin in Logger Group"
+            )
             sys.exit()
-
         if get_me.last_name:
             self.name = get_me.first_name + " " + get_me.last_name
         else:
             self.name = get_me.first_name
-
         LOGGER(__name__).info(f"MusicBot Started as {self.name}")
