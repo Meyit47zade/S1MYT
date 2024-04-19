838884891c782aa2668009c6660573b4da1598b1
Meyit47zade <166849634+Meyit47zade@users.noreply.github.com>
2024-04-19 05:49:48 +0300

    Update bot.py

--- YukkiMusic/core/bot.py
+++ YukkiMusic/core/bot.py
@@ -1,17 +1,16 @@
 import sys
 
 from pyrogram import Client
-from pyrogram.types import BotCommand
 
 import config
 
 from ..logging import LOGGER
 
 
-class Yukki(Client):
+class YukkiBot(Client):
     def __init__(self):
-        LOGGER(__name__).info(f"Starting Bot")
-        app.__init__(
+        LOGGER(__name__).info(f"Bot Başlatıldı")
+        super().__init__(
             "YukkiMusicBot",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
@@ -19,46 +18,27 @@ class Yukki(Client):
         )
 
     async def start(self):
-        await app.start()
+        await super().start()
         get_me = await self.get_me()
         self.username = get_me.username
         self.id = get_me.id
         try:
             await self.send_message(
-                config.LOG_GROUP_ID, "Bot Started"
+                config.LOG_GROUP_ID, "Bot Başlatıldı"
             )
         except:
             LOGGER(__name__).error(
-                "Bot has failed to access the log Group. Make sure that you have added your bot to your log channel and promoted as admin!"
+                "Bot, günlük Grubuna erişemedi. Botunuzu günlük kanalınıza eklediğinizden ve yönetici olarak terfi ettirdiğinizden emin olun!"
             )
             sys.exit()
-        if config.SET_CMDS == str(True):
-            try:
-                await self.set_bot_commands(
-                    [
-                        BotCommand("ping", "Check that bot is alive or dead"),
-                        BotCommand("play", "Starts playing the requested song"),
-                        BotCommand("skip", "Moves to the next track in queue"),
-                        BotCommand("pause", "Pause the current playing song"),
-                        BotCommand("resume", "Resume the paused song"),
-                        BotCommand("end", "Clear the queue and leave voice chat"),
-                        BotCommand("shuffle", "Randomly shuffles the queued playlist."),
-                        BotCommand("playmode", "Allows you to change the default playmode for your chat"),
-                        BotCommand("settings", "Open the settings of the music bot for your chat.")
-                        ]
-                    )
-            except:
-                pass
-        else:
-            pass
         a = await self.get_chat_member(config.LOG_GROUP_ID, self.id)
         if a.status != "administrator":
             LOGGER(__name__).error(
-                "Please promote Bot as Admin in Logger Group"
+                "Lütfen Bot'u Logger Group'ta Yönetici olarak tanıtın"
             )
             sys.exit()
         if get_me.last_name:
             self.name = get_me.first_name + " " + get_me.last_name
         else:
             self.name = get_me.first_name
-        LOGGER(__name__).info(f"MusicBot Started as {self.name}")
+        LOGGER(__name__).info(f"MusicBot olarak başladı {self.name}")
