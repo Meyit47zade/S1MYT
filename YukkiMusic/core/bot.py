65668805c85af3a80861bd891c24b7a55d7bf0cf
Meyit47zade <166849634+Meyit47zade@users.noreply.github.com>
2024-04-19 06:49:01 +0300

    Update bot.py

--- YukkiMusic/core/bot.py
+++ YukkiMusic/core/bot.py
@@ -9,7 +9,7 @@ from ..logging import LOGGER
 class YukkiBot(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Bot Başlatıldı")
-        Yukki().__init__(
+        super().__init__(
             "YukkiMusicBot",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
@@ -17,7 +17,7 @@ class YukkiBot(Client):
         )
 
     async def start(self):
-        await Yukki().start()
+        await super.start()
         get_me = await self.get_me()
         self.username = get_me.username
         self.id = get_me.id
